ca1e0f78a10dba64d7fce324c805c5e758880887
Eugene Karanda <eugene.karanda@outlook.com>
2022-04-08 00:34:54 +0300

    Add dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    entry: './src/index.js',
+module.exports = { //TODO config for prod
+    mode: 'development',
+    entry: './src/SurvivalGame.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+    devtool: 'inline-source-map',
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Survival Game',
