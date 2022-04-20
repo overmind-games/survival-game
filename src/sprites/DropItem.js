96018f8c117caf5e88b93360e446dd9545234b13
Eugene Karanda <eugene.karanda@outlook.com>
2022-04-21 01:01:45 +0300

    add pickup sound

--- src/sprites/DropItem.js
+++ src/sprites/DropItem.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import itemsUrl from '../assets/items.png'
+import pickupSoundUrl from "../assets/audio/pickup.mp3";
 
 const textures = {
     'log': 272,
@@ -14,11 +15,13 @@ export default class DropItem extends Phaser.Physics.Matter.Sprite {
             frameWidth:32,
             frameHeight:32
         });
+
+        scene.load.audio('pickup', pickupSoundUrl);
     }
 
     constructor(scene, type, x, y) {
         super(scene.matter.world, x, y, 'items', textures[type]);
-        this.scene.add.existing(this);
+
         this.setStatic(true);
         this.setScale(0.5);
 
@@ -30,5 +33,12 @@ export default class DropItem extends Phaser.Physics.Matter.Sprite {
 
         this.setExistingBody(circleCollider);
         this.setMass(50);
+
+        this.pickupSound = this.scene.sound.add('pickup');
+        this.scene.add.existing(this);
+    }
+
+    pickup() {
+        this.pickupSound.play();
     }
 }
